4a5ab277f4dec411eac1ef87c2947cfb568cc7e2
Mohana-j <mohanapriyajayakumar25@gmail.com>
2025-02-14 09:23:54 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard'; // Your Dashboard component
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/task2/" element={<Login />} />
+        <Route path="/task2/Dashboard" element={<Dashboard />} />
+        {/* <Route path="/task2" element={<Task2 />} /> Ensure this exists */}
       </Routes>
     </Router>
   );
