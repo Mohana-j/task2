aa85fcce8c763e8379141140449ec760340c92c7
Mohana-j <mohanapriyajayakumar25@gmail.com>
2025-02-14 08:52:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard'; // Your Dashboard component
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
